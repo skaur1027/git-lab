49f89adcb8f05eca17e738d0661a7a12232192b0
skaur1027 <malhotra.sandeepk@gmail.com>
2022-06-10 12:16:07 -0700

    add docstrings

--- name_to_intial.py
+++ name_to_intial.py
@@ -1,4 +1,5 @@
 def name_to_intial(name):
+    """ Function that takes in first name and returns their intials as strings"""
     name_list = list(name.strip().split())
     for name in name_list:
         return str(name[0] + name[0])
